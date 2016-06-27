eeb90493a832f2e3939143cb785fe701c8f032bf
Iridius <irde@mail.ru>
2016-06-27 17:10:18 +0300

    F: Config-file.

---
+++ src/controller/config.java
@@ -0,0 +1,111 @@
+package controller;
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Properties;
+
+public class Config {
+
+    private final static String CASHED_CATALOGS = "CashedCatalogs";
+    private final static String CASHED_USERS = "CashedUsers";
+    private final static String DELIMITER = ";";
+
+    public static void addCatalog(String catalog) {
+        add(CASHED_CATALOGS, catalog, false);
+    }
+
+    public static Collection<String> getCatalogs() {
+        String cashedCatalog = get(CASHED_CATALOGS);
+        return split(cashedCatalog);
+    }
+
+    public static void addUsers(String login) {
+        add(CASHED_USERS, login, false);
+    }
+
+    public static Collection<String> getUsers(){
+        String cashedUsers = get(CASHED_USERS);
+        return split(cashedUsers);
+    }
+
+    private static Collection<String> split(String value){
+        Collection<String> result = new ArrayList<>();
+        if(value != null){
+            String[] parts = value.split(DELIMITER);
+            return Arrays.asList(parts);
+        }
+
+        return result;
+    }
+
+    private static String get(final String path) {
+        Properties properties = new Properties();
+        FileInputStream fis = null;
+
+        File config = getConfig();
+
+        try {
+            fis = new FileInputStream(config);
+            properties.load(fis);
+            fis.close();
+
+            return properties.getProperty(path);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private static void add(String path, String value, boolean overwrite) {
+        Properties properties = new Properties();
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+
+        File config = getConfig();
+
+        try {
+            fis = new FileInputStream(config);
+            properties.load(fis);
+
+            if(!overwrite){
+                String cashedValue = properties.getProperty(path);
+
+                if(cashedValue != null){
+                    value = split(cashedValue).contains(value) ? cashedValue : value + ";" + cashedValue;
+                }
+            }
+
+            properties.setProperty(path, value);
+
+            fos = new FileOutputStream(config);
+            properties.store(fos, "");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fis.close();
+                fos.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private static File getConfig() {
+        File config = new File("config.cfg");
+        if(!config.exists()){
+            try {
+                config.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return config;
+    }
+}
